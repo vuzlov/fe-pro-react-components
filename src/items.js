41ed572f03074d5a1e03c5917f2682394ad26589
evgen_vuzlov <evuzlov@gmail.com>
2022-12-28 01:30:19 +0200

    hw19

---
+++ src/items.js
@@ -0,0 +1,95 @@
+export const items = [
+    {
+        id: 1,
+        label: `15 полезных однострочных выражений JavaScript`,
+        text: `Улучшение JavaScript-кода за пару минут перевод 15 Helpful JavaScript One-Liners`,
+    },
+
+    {
+        id: 2,
+        label: `Полиморфизм простыми словами`,
+        text: `Скорее всего вы уже встречались с понятием “полиморфизм” и даже помните примеры с наследованием, но они показывают далеко не всё`,
+    },
+
+    {
+        id: 3,
+        label: `Начало работы с Node.js`,
+        text: `Node Hero: Глава 1`,
+    },
+
+    {
+        id: 4,
+        label: `Нейросети с нуля для JavaScript разработчиков (Часть 1: Перцептрон)`,
+        text: `Перевод статьи Elyx0: Neural networks from scratch for Javascript linguists (Part1 The Perceptron).`,
+    },
+
+    {
+        id: 5,
+        label: `8 практичных хитростей веб-разработчика применить немедленно!`,
+        text: `HTML, CSS и JS хинты, которые полезно знать перевод 8 Tricks for Web Developers You Can Put Into Practice Immediately`,
+    },
+
+    {
+        id: 6,
+        label: `Насколько JavaScript сильный?`,
+        text: `Погружение в тему неявных преобразований. Где и как возникают и вычисляются неявные преобразования в JavaScript?`,
+    },
+
+    {
+        id: 7,
+        label: `Изучите CSS-переменные за 5 минут`,
+        text: `Перевод статьи Per Harald Borgen: Learn CSS Variables in 5 minutes.`,
+    },
+
+    {
+        id: 8,
+        label: `Создание MEVN-приложения (Часть 1/2)`,
+        text: `Создание full-stack приложения на основе Vue.js и Express.js (+MongoDB)`,
+    },
+
+    {
+        id: 9,
+        label: `Реактивность Vue.js`,
+        text: `Реактивность фреймворка Vue.js и использование метода Vue.set`,
+    },
+
+    {
+        id: 10,
+        label: `Микроптимизации производительности и JavaScript`,
+        text: `Недавно в комментариях на Hexlet я поучаствовал в дискуссии на тему «Что важнее — оптимизации производительности или качество кода?» Если…`,
+    },
+
+    {
+        id: 11,
+        label: `Девшахта-подкаст`,
+        text: `#108: Производительность кода — это важнейший критерий качества?`,
+    },
+    {
+        id: 12,
+        label: `Одна из самых красивых идей в информатике: Y-Combinator`,
+        text: `Перевод статьи Lucas Fernandes da Costa “Y: The Most Beautiful Idea in Computer Science explained in JavaScript”`,
+    },
+];
+
+export const navList = [
+    {
+        id: 1,
+        link: `/`,
+        text: `Home`
+    },
+    {
+        id: 2,
+        link: `/#about`,
+        text: `About us`
+    },
+    {
+        id: 3,
+        link: `/#contact`,
+        text: `Contact us`
+    },
+    {
+        id: 4,
+        link: `/#help`,
+        text: `Help page`
+    },
+];
